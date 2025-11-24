ad73befe4f2868b94edaf14505d917d3157d1782
Diego Emilio Muñiz Ramirez <diego.munizr@udem.edu>
2025-11-24 12:22:47 -0600

    CategoriaEspecifica.jsx

--- CategoriaEspecifica.jsx
+++ CategoriaEspecifica.jsx
@@ -5,21 +5,20 @@ import '../styles/stylesC.css';
 const CategoriaEspecifica = () => {
   const { nombreCategoria } = useParams();
 
-  // Datos de ejemplo para productos por categoría
   const productosPorCategoria = {
   salsas: [
     { 
       id: 1, 
       nombre: "Salsa Verde", 
       precio: "100.00", 
-      imagen: "/Imagenes/Salsa Verde.png",  // ← Así
+      imagen: "/Imagenes/Salsa Verde.png",
       alt: "Salsa Verde" 
     },
     { 
       id: 2, 
       nombre: "Chimichurri", 
       precio: "150.00", 
-      imagen: "/Imagenes/Chimichurri.png",  // ← Así
+      imagen: "/Imagenes/Chimichurri.png",
       alt: "Chimichurri" 
     }
   ],
